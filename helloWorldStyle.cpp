6a71989eb4589acfc4dcb70d2ebbd0067d8460b0
Zak Goneau <zak.goneau@stu.ocsb.ca>
2023-09-18 16:55:09 +0000

    Hello, World with proper style in C++.

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Zak Goneau All rights reserved.
+//.
+// Created By: Zak Goneau
+// Date: Sep. 18, 2023
+// This program displays "Hello, World with style".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with proper style!" << std::endl;
+    std::cout << "C++ is a bit more complicated than Python." << std::endl;
+}
